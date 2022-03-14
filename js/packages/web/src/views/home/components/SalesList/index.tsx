888f2288b4820f0977beeb838587e7876e8011d2
danbokhari98 <danbokhari98@gmail.com>
2022-03-14 11:36:32 -0400

    SECOND COMMIT

--- js/packages/web/src/views/home/components/SalesList/index.tsx
+++ js/packages/web/src/views/home/components/SalesList/index.tsx
@@ -43,9 +43,9 @@ export const SalesListView = (props: { collectionMintFilter?: string }) => {
     <>
       {!props.collectionMintFilter && (
         <Banner
-          src="/main-banner.svg"
-          headingText="The amazing world of Metaplex."
-          subHeadingText="Buy exclusive Metaplex NFTs."
+          src="/main-banner.png"
+          headingText="SPP Auction House"
+          subHeadingText="Spend Your $TREATS Wisely"
           actionComponent={<HowToBuyModal buttonClassName="secondary-btn" />}
           useBannerBg
         />
